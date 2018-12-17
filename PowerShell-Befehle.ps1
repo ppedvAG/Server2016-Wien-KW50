ba3e241fda2001590b482570e92c4fd11f751961
Administrator <stefan.ober@shitnet.de>
2018-12-17 17:03:44 +0100

    add computer

--- PowerShell-Befehle.ps1
+++ PowerShell-Befehle.ps1
@@ -3,3 +3,12 @@ Rename-Computer -NewName Server1 -Restart
 
 #registrieren eines Geräts beim DNS
 ipconfig /registerdns
+
+#lokalen dns cache löschen
+Clear-DnsClientCache      
+
+#dns server
+Clear-DnsServerCache     
+
+#domain join und rename
+Add-Computer -DomainName ppedv.test -NewName Server2 -Restart -Credential ppedv\Administrator
